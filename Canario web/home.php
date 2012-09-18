8668ad664732eb48e77258e44a2701e34fed995d
Agustin Breit <agustinbreit@gmail.com>
2012-09-18 04:47:00 -0300

    Creacion de index y home de prueba

---
+++ Canario web/home.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="css/bootstrap-responsive.css" rel="stylesheet" type="text/css" media="all">
+<link href="img/favicon.ico" rel="shortcut icon" type="image/x-icon">
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+  <script src="js/modernizr-2.6.1-respond-1.1.0.min.js"></script>
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<title>Canario!!!</title></head>
+
+<body>
+
+<div class="container">
+ 
+<div class="navbar navbar-fixed-top	">
+	<div class="navbar-inner">
+    	<div class="container">
+        	<ul class="nav nav-pills">
+            <a class="brand" href="#">Canario Web</a>
+        	<li class="active">
+            	<a href="home.html"><i class="icon-home"></i>  Home</a>
+            </li>
+			<li>
+            	<a href="#"><i class="icon-white icon-user"></i>  Perfil</a>
+            </li>
+          
+            </ul>
+           <div class="pull-left"> 
+          <form class="navbar-form"><input type="text" class="search-query span3 " placeholder="Buscar">
+          
+          </form>   </div>
+        </div>
+    </div>
+</div> 
+
+ <div class="contenedor">
+ 	<div class="pensando">
+    	
+        
+  <div class="container-fluid">  
+     <div class="accordion" id="accordion2">  
+            <div class="accordion-group">  
+              <div class="accordion-heading">  
+                 <button class=" btn btn-large btn-success accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">Canarear</button>  
+                   
+                </textarea>  
+              </div>  
+              <div id="collapseOne" class="accordion-body collapse" style="height: 0px; ">  
+                <div class="accordion-inner">  
+                   <textarea cols="40" rows="5" id="textarea" maxlength="150" placeholder="canarear!!!"></textarea> 
+        <br><br><button class="btn btn-primary">Canarear</button>
+        <div id="caracteres"></div>
+                </div>  
+              </div>  
+            </div>  
+            
+    </div>  
+  </div>        
+        
+        
+			   </div>
+    <div class="seguidores"><h4><br><br>los usuarios a los que sigo<br><br><br></h4></div>
+ 	<div class="usuariosact"><h4><br><br>el top 10 de los usuarios mas activos<br><br><br><br></h4></div>
+
+ </div>
+ 	<div class="mensajes"><h1><br>el top 10 en tiempo real de los mensajes de los usuarios<br><br><br><br><br><br><br><br><br><br><br><br></h1></div> 
+  <!-- end .container --></div>
+  
+  
+   <script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/textarea.js"></script>         
+	<script type="text/javascript" src="js/bootstrap.collapse.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
