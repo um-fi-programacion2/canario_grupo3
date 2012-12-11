87f98c971e3a2c5a9540f7f5f7f9d322d3b46420
Agustin Breit <agustinbreit@gmail.com>
2012-12-11 18:55:22 -0300

    funcion para agregar notificaciones

---
+++ CanarioWeb/src/java/dao/GuardarFlagsDao.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import com.opensymphony.xwork2.ActionContext;
+import java.util.Map;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author el pampa
+ */
+public class GuardarFlagsDao {
+    public static boolean guardarFlagsDao(Long idu, String flag1, String flag2) {
+                Transaction t = null;
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+                Session s = sf.openSession();
+                Map auth = ActionContext.getContext().getSession();
+                           
+		
+                Query query = s.createQuery("UPDATE Usuarios set flag1 = :flag1 and flag2 = :flag2 where idu= :idu");
+                
+                query.setParameter("idu",((Number)auth.get("idusuario")).longValue());
+                query.setParameter("flag1", flag1.toString());
+                query.setParameter("flag2", flag2.toString());
+                
+                try {
+                    t = s.beginTransaction();
+           query.executeUpdate();
+           t.commit(); // commit transaction
+                return true;
+		} catch (Exception ex) {
+			System.err.println("Error -->" + ex.getMessage());
+			if (t != null) {
+                        t.rollback();
+                    }
+			return false;
+		}
+        }
+}
---
+++ CanarioWeb/src/java/view/GuardarFlagsAction.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import com.opensymphony.xwork2.ActionSupport;
+import dao.GuardarFlagsDao;
+import model.Usuarios;
+
+/**
+ *
+ * @author el pampa
+ */
+public class GuardarFlagsAction extends ActionSupport{
+    private String flag1;
+    private String flag2;
+    private Long idu;
+
+    public Long getIdu() {
+        return idu;
+    }
+
+    public void setIdu(Long idu) {
+        this.idu = idu;
+    }
+
+    public String getFlag1() {
+        return flag1;
+    }
+
+    public void setFlag1(String flag1) {
+        this.flag1 = flag1;
+    }
+
+    public String getFlag2() {
+        return flag2;
+    }
+
+    public void setFlag2(String flag2) {
+        this.flag2 = flag2;
+    }
+    
+    public String excecute() throws Exception{
+       
+        if(GuardarFlagsDao.guardarFlagsDao(idu, flag1,flag2)){
+            return "success";
+        }
+            
+        else{
+        return "fail";
+        }
+    }
+    
+}
